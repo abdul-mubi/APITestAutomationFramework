86ae7de9b341faf455e47d6b5f2a895f96e6dc49
ahameed <ahameed@opentext.com>
2026-08-01 20:47:06 +0530

    Added logic for handling multiple environment and updated jave to 14 for arrow operator usage

--- src/test/java/com/api/tests/LoginApiTest.java
+++ src/test/java/com/api/tests/LoginApiTest.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 import com.api.pojo.UserCredentials;
+import static com.api.util.ConfigManager.*;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
@@ -12,8 +13,9 @@ public class LoginApiTest {
 	@Test
 	public void loginAPITest() {
 		UserCredentials userCredentials = new UserCredentials("iamfd", "password");
+		
 		Response response = given()
-			.baseUri("http://64.227.160.186:9000/v1")
+			.baseUri(getProperty("BASE_URI"))
 			.and()
 			.contentType(ContentType.JSON)
 			.and()
---
+++ src/test/java/com/api/util/ConfigManager.java
@@ -0,0 +1,34 @@
+package com.api.util;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+	
+	private static Properties prop = new Properties();
+	private static String path = "/config/config.properties";
+	private static String env;
+	static {
+		env = System.getProperty("env","qa");
+		switch(env) {
+		case "dev" -> path = "/config/config.dev.properties";
+		case "qa" -> path = "/config/config.qa.properties";
+		case "uat" -> path = "/config/config.uat.properties";
+		default -> path = "/config/config.qa.properties";
+		}
+		InputStream in = ConfigManager.class.getResourceAsStream(path);
+		if (in == null) {
+			throw new RuntimeException("File is not found in this path - "+path);
+		}
+		try  {
+			prop.load(in);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getProperty(String key) {
+		return prop.getProperty(key);
+	}
+
+}
